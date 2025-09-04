a82851732d82bc174c95819c7ca540bb1bb2278c
braian <braianalejandromarinmontoya@gmail.com>
2025-09-04 14:34:28 -0500

    se crea header(nav)

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
+import "./nav.css";
 
 export const Nav = () => {
   return (
-    <div className="navar">
+      <nav className="navbar-menu">
         <h1>URBAN THREADS</h1>
-        <a href="">Inicio</a>
-        <a href="">Productos</a>
-        <a href="">Contacto</a>
-    </div>
-    
-  )
-}
+        <ul>
+          <li><a href="#">Inicio</a></li>
+          <li><a href="#">Productos</a></li>
+          <li><a href="#">Contacto</a></li>
+        </ul>
+      </nav>
+  );
+};
 
-export default Nav
+export default Nav;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Nav } from './components/nav'
+import { PaginaPrincipal } from './pages/paginaPrincipal'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Nav />
+    <div className="pagina-p">
+      <PaginaPrincipal />
+    </div>
   </StrictMode>,
 )
