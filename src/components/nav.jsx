0d3b5c39b8734a243498062a39b4c1b32c523893
braian <braianalejandromarinmontoya@gmail.com>
2025-09-04 13:28:45 -0500

    se crea encarpetado de la pagina

---
+++ src/components/nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Nav = () => {
+  return (
+    <div className="navar">
+        <h1>URBAN THREADS</h1>
+        <a href="">Inicio</a>
+        <a href="">Productos</a>
+        <a href="">Contacto</a>
+    </div>
+    
+  )
+}
+
+export default Nav
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { Nav } from './components/nav'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Nav />
   </StrictMode>,
 )
