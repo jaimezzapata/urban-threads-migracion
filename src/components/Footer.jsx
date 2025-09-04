1d968b301d6f035b82f86b86c706d55b79ed66b5
Yeison Zapata <yeisonvrx98@gmail.com>
2025-09-04 13:44:37 -0500

    Se crea estructura con componentes Nav y Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+    <div>
+        <h3>URBAN THREADS</h3>
+        <p>Redefiniendo el estilo urbano con diseños minimalistas y calidad premium.</p>
+    </div>
+    <div>
+
+    </div>
+    <div>
+
+    </div>
+    <div>
+
+    </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav>
+    <div>
+        <h1>URBAN THREADS</h1>
+        <li>
+        <a href="">Inicio</a>
+        <a href="">Productos</a>
+        <a href="">Contacto</a>
+        </li>
+    </div>
+    </nav>
+  )
+}
+
+export default Nav
