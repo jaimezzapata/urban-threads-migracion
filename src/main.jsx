1adfee2fa70abaf884e360b7f3a84ae1f50ea165
Sebastian Arboleda <juansebastian_gomez456@hotmail.com>
2025-09-04 13:37:01 -0500

    Se organiza el proyecto para la migración y se encarpeta

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import  Home  from './pages/Home'
+import  Contacto  from './pages/Contacto'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Home />
+    <Contacto />
   </StrictMode>,
 )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import NavbarUrban from "../components/NavbarUrban";
+
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <NavbarUrban /> 
+        </div>
+    )
+}
+
+export default Home 
